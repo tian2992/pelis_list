6e1a447c751d6b45695fbadcd8ad6688ab41cadb
Sebastian Oliva <code@sebastianoliva.com>
2020-04-24 15:17:58 -0600

    search form query

--- pelis/mooviee/urls.py
+++ pelis/mooviee/urls.py
@@ -1,9 +1,11 @@
-from .views import AllMovies, ModernMovies
+from .views import AllMovies, ModernMovies, DumbMovieSearch
+    # , dumb_movie_search
 
 from django.urls import path
 
 
 urlpatterns = [
-    path("/", AllMovies.as_view(), name='all_movies' ),
-    path('/modern-movie', ModernMovies.as_view(), name = "recent_movies")
+    path("", AllMovies.as_view(), name='all_movies' ),
+    path('modern-movie', ModernMovies.as_view(), name = "recent_movies"),
+    path('dumb-search', DumbMovieSearch.as_view(), name="bad-movie-search")
 ]
--- pelis/mooviee/views.py
+++ pelis/mooviee/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views import View
+from .models import Movie
 
-from pelis.mooviee.models import Movie
 
 
+class SingleMovie(DetailView):
+    pass
 
 class AllMovies(ListView):
     '''Todas las peli'''
     model = Movie
+    template_name = 'mooviee/movie_list.html'
 
 
 class ModernMovies(ListView):
@@ -20,4 +24,39 @@ class ModernMovies(ListView):
 
     ## esto da todos los movie con Greaterh Than or Equal 2000
     queryset = Movie.objects.filter(year__gte=2000)
+    template_name = 'mooviee/movie_list.html'
 
+
+class DumbMovieSearch(View):
+    base_query = Movie.objects
+
+    def do_filter(self, arg):
+        return DumbMovieSearch.base_query.filter(title__contains=arg)
+
+    def get(self, request):
+        return render(request, "mooviee/dumb_search.html")
+
+    def post(self, request):
+        moov = request.POST.get("movie_n")
+        if moov == "":
+            return 404
+        # objects es un queryset buscando todos los valores de movie_n
+        # objects = Movie.objects.filter(title__contains=moov)
+        objects = self.do_filter(moov)
+        return render(request, "mooviee/dumb_search.html", context=dict(object_list=objects))
+
+
+##
+#class AnimalAppointment():
+#    created_time = datetime.now()
+#    def check_available()
+#
+# class WebAppointment(AnimalAppointment, View):
+#     def GET
+#
+
+#def dumb_movie_search(request):
+#    post_data = request.POST
+#    get_no = request.GET
+#
+#    render("dumb_search.html")
